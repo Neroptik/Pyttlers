4c9977859c98c05b738b8d09a6699ecb857945e6
Neroptik <neroptik@yopmail.com>
2012-11-22 14:59:12 +0100

    some tests

--- src/Pyttlers.py
+++ src/Pyttlers.py
@@ -4,5 +4,20 @@ Created on 22 nov. 2012
 @author: VJLT0530
 '''
 
+import pyamf.remoting
+
+class Test():
+    def __init__(self, a, b, c):
+        '''
+        Test class initialization
+        :param a:toto
+        :param b:tata
+        :param c:titi
+        '''
+        self.a = a
+        self.b = b
+        self.c = c
+    
 if __name__ == '__main__':
-    pass
+    envelope = pyamf.remoting.Envelope(3)
+    print envelope
